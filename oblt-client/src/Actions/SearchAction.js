f921e061b27c749ae7d74ec638d4494d30a71497
Barış Can Nazlıcan <64064136+wulfharth7@users.noreply.github.com>
2024-11-22 12:59:09 +0300

    Fixed Date Departure bug

--- oblt-client/src/Actions/SearchAction.js
+++ oblt-client/src/Actions/SearchAction.js
@@ -1,30 +1,35 @@
 export const handleSearch = (
-    value1,
-    value2,
-    departureDate,
-    navigate
-  ) => {
-    if (!value1 || !value2) {
-      alert('Please select both origin and destination locations.');
-      return;
-    }
-  
-    // Save the current values to localStorage
-    localStorage.setItem('lastOrigin', JSON.stringify(value1));
-    localStorage.setItem('lastDestination', JSON.stringify(value2));
-    localStorage.setItem('lastDepartureDate', departureDate);
-  
-    // Construct the URL
-    const originId = value1.id;
-    const destinationId = value2.id;
-    const dynamicPath = `${originId}-${destinationId}`;
-  
-    navigate(`/seferler/${dynamicPath}/${departureDate}`, {
-      state: {
-        origin: value1,
-        destination: value2,
-        departureDate: departureDate,
-      },
-    });
-  };
-  
+  value1,
+  value2,
+  departureDate,
+  navigate
+) => {
+  if (!value1 || !value2) {
+    alert('Please select both origin and destination locations.');
+    return;
+  }
+
+  const today = new Date();
+  const selectedDate = new Date(departureDate);
+
+  if (selectedDate < today) {
+    alert('Departure date cannot be older than today.');
+    return;
+  }
+
+  localStorage.setItem('lastOrigin', JSON.stringify(value1));
+  localStorage.setItem('lastDestination', JSON.stringify(value2));
+  localStorage.setItem('lastDepartureDate', departureDate);
+
+  const originId = value1.id;
+  const destinationId = value2.id;
+  const dynamicPath = `${originId}-${destinationId}`;
+
+  navigate(`/seferler/${dynamicPath}/${departureDate}`, {
+    state: {
+      origin: value1,
+      destination: value2,
+      departureDate: departureDate,
+    },
+  });
+};
